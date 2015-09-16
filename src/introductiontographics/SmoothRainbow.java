30bc0e8bf64831e0a7fe6c7a4fd49972d92d3ca8
kasrasadeghi <kasra.sadeghi1@gmail.com>
2015-09-15 21:41:30 -0500

    1.4

--- src/introductiontographics/SmoothRainbow.java
+++ src/introductiontographics/SmoothRainbow.java
@@ -17,7 +17,7 @@ import java.awt.Graphics;
 public class SmoothRainbow implements View<Integer>
 {
     
-    double freq = .2;
+    double freq = .01;
     @Override
     public void paint(Integer m, Graphics g, int w, int h) 
     {
@@ -28,29 +28,36 @@ public class SmoothRainbow implements View<Integer>
         int cx = w/2;
         int cy = h/2;
         
-        for (int i = 0; i < 100; ++i) {
+        int temp = 600;
+        for (int i = temp + r/8; i > -1; --i) {
             Color phaser = new Color(getRed(i), getGreen(i), getBlue(i));
             g.setColor(phaser);
-            g.fillRect(i*10, 0, 10, 50 );
+            int dr = r * i/ temp;
+            //g.fillRect(cx - dr - r/8, cy - r, 1, 1000 );
+            
+            g.fillOval(cx - dr - r/8, cy - dr - r/8, 2*dr + r/4, 2*dr + r/4);
         }
+        g.setColor( Color.WHITE);
+        g.fillOval(cx - r/8, cy - r/8, r/4, r/4);
+        g.clearRect(cx - r - r/2, cy, 2*r + r, 2*r + r);
         
     }
-    double c = 0.3;
+    double c = 2.1;
     public int getRed(int i )
     {
         double a = (double)(i);
-        return (int)(Math.sin(freq*a+0 + c)*(127)+127);
-    }
-    
-    public int getBlue(int i )
-    {
-        double a = (double)(i);
-        return (int)(Math.sin(freq*a + c + 4*Math.PI/3.0)*127+127);
+        return (int)(Math.sin(freq*a + c)*100 + 150);
     }
     
     public int getGreen(int i)
     {
         double a = (double)(i);
-        return (int)(Math.sin(freq*a + c + 2*Math.PI/3.0)*127+127);
+        return (int)(Math.sin(freq*a + c + 2* Math.PI/3.0)*110 + 140);
+    }
+    
+    public int getBlue(int i )
+    {
+        double a = (double)(i);
+        return (int)(Math.sin(freq*a + c + 4 * Math.PI/3.0)*110 + 140);
     }
 }
